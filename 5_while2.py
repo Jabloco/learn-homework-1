a0b1ec86379448e59b651dde4263d8940a262f01
Jabloco <50886478+Jabloco@users.noreply.github.com>
2021-09-06 21:45:39 +0300

    После общения в чате переписал на выход по команде. Добавил ответ на вопросы, которых  нет в словаре

--- 5_while2.py
+++ 5_while2.py
@@ -19,14 +19,20 @@ questions_and_answers = {
     "Как дела": "Хорошо!",
     "Что делаешь?": "Программирую",
     "Откуда ты?": "Из Матрицы",
-    "Кто такой Гвидо ван Россум":"Великодушный пожизненный диктатор"
+    "Кто такой Гвидо ван Россум": "Великодушный пожизненный диктатор"
 }
 
-def ask_user(input_dict):
+def ask_user(answers_dict: dict):
+    print('Для выхода введите: Хватит')
     user_question = input('Пользователь: ')
-    while user_question in questions_and_answers.keys():
-        print(questions_and_answers[user_question])
+    while user_question != 'Хватит':  # выход по стоп-слову 'Хватит'
+        if user_question in answers_dict.keys():
+            print('Программа: ', end='')
+            print(answers_dict[user_question])
+        else:
+            print('Программа: Мне нечего сказать')
         user_question = input('Пользователь: ')
+    
     
 if __name__ == "__main__":
     ask_user(questions_and_answers)
