c9ac4098185a9a9deb77e4a4c7661e775860a3a2
Jabloco <50886478+Jabloco@users.noreply.github.com>
2021-09-07 09:21:03 +0300

    Седьмое задание работает

--- 7_exception2.py
+++ 7_exception2.py
@@ -13,16 +13,28 @@
     
 """
 
-def discounted(price, discount, max_discount=20)
-    """
-    Замените pass на ваш код
-    """
-    pass
+def discounted(price, discount, max_discount=20):
+    try:
+        price = abs(float(price))
+        discount = abs(float(discount))
+        max_discount = abs(int(max_discount))
+    except (ValueError, TypeError) as error:
+        return f'Что-то пошло не так: {error}'
+    else:
+        if max_discount >= 100:
+            raise ValueError('Слишком большая максимальная скидка')
+        if discount >= max_discount:
+            return price
+        else:
+            return price - (price * discount / 100)
     
+
+
 if __name__ == "__main__":
     print(discounted(100, 2))
     print(discounted(100, "3"))
     print(discounted("100", "4.5"))
     print(discounted("five", 5))
     print(discounted("сто", "десять"))
+    print(discounted(10, "десять"))
     print(discounted(100.0, 5, "10"))
